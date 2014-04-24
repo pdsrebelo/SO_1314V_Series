0c14e7570c0a126637428a13c09dd20640777bfa
Cátia Ormonde <a36923@alunos.isel.pt>
2014-04-24 12:17:58 +0100

    ERGDT

---
+++ Serie1_SO_1314V/Serie1_SO_1314V/Exercicio_4_attachment.c
@@ -0,0 +1,48 @@
+#define _UNICODE
+#define UNICODE
+
+#include <windows.h>
+#include <tchar.h>
+
+#define DATALEN 1024*1024*256U
+
+static BYTE data[DATALEN] = { 1, 2 };
+
+ULONG ReadData()
+{
+	ULONG sum = 0;
+	ULONG i;
+
+	for (i = 0; i < DATALEN; ++i)
+		sum += data[i];
+
+	return sum;
+}
+
+VOID WriteData(int v)
+{
+	ULONG i;
+
+	for (i = 0; i < DATALEN; ++i)
+		data[i] = v;
+}
+
+VOID PressToContinue(LPCTSTR phaseName)
+{
+	_tprintf(_T("Prima RETURN para %s\n"), phaseName);
+	getchar();
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	_tprintf(_T("ProcessId = %d\n"), GetCurrentProcessId());
+	PressToContinue(_T("iniciar"));
+
+	ReadData();
+	PressToContinue(_T("continuar"));
+
+	WriteData(1);
+	PressToContinue(_T("terminar"));
+
+	return 0;
+}
