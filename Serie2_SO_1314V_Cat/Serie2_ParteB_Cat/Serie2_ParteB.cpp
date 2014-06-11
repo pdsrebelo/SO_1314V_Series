9cd31d1e421551bc1bfbecca7270ccbc321deccd
Cátia Ormonde <a36923@alunos.isel.pt>
2014-06-12 00:58:19 +0100

    Ex.1 / Part B - updated

--- Serie2_SO_1314V_Cat/Serie2_ParteB_Cat/Serie2_ParteB.cpp
+++ Serie2_SO_1314V_Cat/Serie2_ParteB_Cat/Serie2_ParteB.cpp
@@ -1,4 +1,5 @@
 #include "Serie2_ParteB.h"
+#include <math.h>
 /*	EXERCÍCIO 1
 
 1. Considere  um  array  de  inteiros  com  dimensão  na  ordem  dos  MBytes.  Pretende­-se  realizar  o 
@@ -9,86 +10,92 @@ parte  alocado  a  uma  thread.  Determine  para  o  seu  sistema,  qual  o  núm
 proporciona  melhores  tempos  de execução do  cálculo do  somatório. Apresente as medições dos 
 tempos para as várias experiências.
 */
-#define BIG_ARRAY_SIZE 1048576 // e.g. Array size: 3 Megabytes... // Note: 1 Megabyte = 1,048,576 bytes = (1024*1024) bytes
-
-LONGLONG bigArray[BIG_ARRAY_SIZE];
-LONGLONG arraySum;
+#define BIG_ARRAY_SIZE 3*1048576 // e.g. Array size: 3 Megabytes... // Note: 1 Megabyte = 1,048,576 bytes = (1024*1024) bytes
+#define NUMBER_OF_TESTS 4
+LONG bigArray[BIG_ARRAY_SIZE];
+LONG resultArray[NUMBER_OF_TESTS];
 
 typedef struct array_positions_for_threads{
 	DWORD begin;
 	DWORD end;
+	DWORD resultIdx; // Index of resultArray - where each thread should add its sum
 }ARRAY_POS,*PARRAY_POS;
 
 unsigned int __stdcall partialArraySum(void * arg){ // Função que vai estar associada a cada thread, para calcular o somatório de uma parte do array
-
-	// Now: Get the index values from the input arg!
 	DWORD i, start, end, accum = 0;
 	PARRAY_POS position = (PARRAY_POS)arg;
+
 	start = position->begin;
 	end = position->end;
+
 	for (i = start; i < end; i++){
 		accum += bigArray[i];
 	}
-	arraySum += accum;
-	return arraySum;
+	resultArray[position->resultIdx] += accum;
+	return resultArray[position->resultIdx];
 }
 
-void Ex1_ParallelArraySum() {
+void Ex1_ParallelArraySum(DWORD nThreads, DWORD resultIdx) {
 
-	//  Uma  vez  que  a  operação  somatório  é  comutativa  e 
-	//  associativa, o array poderá  ser dividido em  várias partes, estando o  cálculo do  somatório de cada 
-	//	parte  alocado  a  uma  thread.
+	DWORD i, arrayPositionsPerThread, extraPositions, 
+		startIdx = 0; // Indexes of the array elements' positions 
 
+	// Define the number of array positions that each thread will be responsible for counting
+	arrayPositionsPerThread = BIG_ARRAY_SIZE / nThreads;// Divide the work by the threads
+	extraPositions = BIG_ARRAY_SIZE % nThreads;			// The rest of the division result
+
+	for (i = 0; i < nThreads; i++){
+		DWORD begin, end;
+		PARRAY_POS arrayPositions = (PARRAY_POS)malloc(sizeof(ARRAY_POS));
+
+		begin = startIdx;
+		end = begin + arrayPositionsPerThread;
+
+		if (extraPositions>0){
+			end += 1;
+			extraPositions -= 1;
+		}
+		
+		// Create the thread that will get the sum of the elements in the specified array bounds
+		// a struct is needed to pass the two arguments to the "partialArraySum" function
+		arrayPositions->end = end;
+		arrayPositions->begin = begin;
+		arrayPositions->resultIdx = resultIdx;
+
+		_beginthreadex(NULL, 0, partialArraySum, (VOID*)(arrayPositions), 0, NULL);
+
+		startIdx = end;
+	}
+
+}
+
+int main() {
+
+	// First Test: nWorkingThreads = (2^0) * number of processors
 	SYSTEM_INFO sysinfo;
-	DWORD i, nCpu, nThreads, arrayPositionsPerThread, extraPositions;
-	DWORD startIdx = 0; // Indexes of the array elements' positions 
-
-	// Initialize the "arraySum" variable
-	arraySum = 0;
+	DWORD nCpu, nThreads;
+	int i;
 
 	// Initialize the array with the values to sum
 	for (i = 0; i < BIG_ARRAY_SIZE; i++){
 		bigArray[i] = rand() % 100 + 1;		// Random number between 1 and 100
 	}
 
-	// Get the number of processor units
 	GetSystemInfo(&sysinfo);
 	nCpu = sysinfo.dwNumberOfProcessors;
-
-	// Define the number of threads that we want to create
 	nThreads = nCpu;
 
-	// Define the number of array positions that each thread will be responsible for counting
-	arrayPositionsPerThread = BIG_ARRAY_SIZE / nThreads;// Divide the work by the threads
-	extraPositions = BIG_ARRAY_SIZE % nThreads;			// The rest of the division result
-
-	// Starting with one thread per processor: ...
-
-	for (i = 0; i < nThreads; i++){	// For each cpu
-		DWORD begin, end;
-		PARRAY_POS arrayPositions = (PARRAY_POS)malloc(sizeof(ARRAY_POS));
-
-		begin = startIdx;
-		end = begin + arrayPositionsPerThread - 1;
-
-		if (extraPositions>0){
-			end += 1;
-			extraPositions -= 1;
-		}
-		startIdx = end + 1;
-
-		// Create the thread that will get the sum of the elements in the specified array bounds
-		// a struct is needed to pass the two arguments to the "partialArraySum" function
-		arrayPositions->end = end;
-		arrayPositions->begin = begin;
-		_beginthreadex(NULL, 0, partialArraySum, (VOID*)(arrayPositions), 0, NULL);
+	for (i = 0; i < NUMBER_OF_TESTS; i++){ // {(2^i) * number of processors} threads, per each CPU
+		nThreads = pow((double)2, i) * nCpu;
+		resultArray[i] = 0;
+		Ex1_ParallelArraySum(nThreads, i);
+		printf("\n::::: Test With %d threads :::::", nThreads);
+		printf("\nArray Sum = %d\n", resultArray[i]);
 	}
+
+	//TODO
 	//	Determine  para  o  seu  sistema,  qual  o  número  de  threads  que 
 	//	proporciona  melhores  tempos  de execução do  cálculo do  somatório.Apresente as medições dos 
 	//	tempos para as várias experiências.
-}
-
-int main() {
-	// TODO: Test Ex1_ParallelArraySum
-	Ex1_ParallelArraySum();
+	getchar();
 }
