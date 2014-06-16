0b98ea208623520ac9c40588add905ef6d1c2594
Cátia Ormonde <a36923@alunos.isel.pt>
2014-06-16 15:01:02 +0100

    Ex.2 in progress

--- BackupServer_C/BackupServerDll/BackupServer.h
+++ BackupServer_C/BackupServerDll/BackupServer.h
@@ -1,25 +1,36 @@
 ﻿#include <windows.h>
 #include <process.h>
 #include <stdio.h>
-typedef struct H_BACKUP_SERVICE{}HBACKUPSERVICE, *PBACKUPSERVICE;
 
-typedef enum BACKUP_OPERATION{
+#define MAX_REQUESTS_NR 128		//número máximo de pedidos
+
+typedef BOOL(*ProcessorFunc)(PBACKUPENTRY pentry);
+
+typedef enum BACKUP_OPERATION{	//tipos de operações que se podem realizar sobre os ficheiros
 	backup,
 	restore,
 	exit
 };
 
-/*
-Cada  entrada  de  pedido (do  tipo  BACKUPENTRY, a  definir)  deve  conter  os  seguintes 
-dados : identificação  do  ficheiro;  Identificador  do  processo  que  originou  o  pedido;  dois  HANDLEs 
-para  indicar  o  resultado  da  operação (sucesso  ou  insucesso);  o  tipo  de  operação  a  realizar 
-(backup, restore e exit).*/
 typedef struct BACKUP_ENTRY{
-	TCHAR * fileName;			//identificação do ficheiro
+	TCHAR * filePath;			//identificação do ficheiro
 	DWORD clientProcessId;		//identificador do processo que originou o pedido
-	HANDLE succes;				//handle para indicar o resultado da operação
+	HANDLE success;				//handle para indicar o resultado da operação
 	HANDLE unsuccess;			//handle para indicar o resultado da operação
-	BACKUP_OPERATION operation;//tipo de operação a realizar
+	BACKUP_OPERATION operation;	//tipo de operação a realizar
 }BACKUPENTRY, *PBACKUPENTRY;
 
-typedef BOOL(*ProcessorFunc)(PBACKUPENTRY pentry);
+typedef struct H_BACKUP_SERVICE{
+	TCHAR fileStoragePath[128];				//caminho para a pasta onde irão ser guardadas as cópias dos ficheiros
+	BACKUPENTRY requests[MAX_REQUESTS_NR];
+	HANDLE hServiceExclusion;				//handle usado para garantir exclusão ao acesso aos dados do serviço
+	DWORD nRequests;						//número de pedidos existentes até ao momento (começa a 0. é usado como idx para os novos pedidos)
+	ProcessorFunc serverRequestProcessor;	//função do servidor, a chamar cada vez que se for processar um pedido
+}HBACKUPSERVICE, *PBACKUPSERVICE;
+
+
+HBACKUPSERVICE CreateBackupService(TCHAR * serviceName, TCHAR * repoPath);
+
+BOOL CloseBackupService(HBACKUPSERVICE service);
+
+BOOL ProcessNextEntry(HBACKUPSERVICE service, ProcessorFunc processor);
--- BackupServer_C/BackupServerDll/BackupServerDll.cpp
+++
@@ -1,6 +0,0 @@
-// BackupServerDll.cpp : Defines the exported functions for the DLL application.
-//
-
-#include "stdafx.h"
-
-
